4d5e4a4b3982569d96f2305d15fff6dbfae15740
Maksym Starodubets <maksim.starodybets@outlook.com>
2025-10-31 11:07:36 +0100

    Praesenzaufgabe 2 und 3

---
+++ src/p2/P2_main.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args){
+        int i = 1;
+        boolean b = False;
+
+        if (i > 10){
+            b = True;
+        }
+    }
+}
